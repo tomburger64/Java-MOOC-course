22cd879d901c53d5f807742453ae4c8ed12713d7
Tom <157364916+tomburger64@users.noreply.github.com>
2024-11-14 14:43:56 +0100

    first console msg

--- Java-programming/Part-1/sandbox.java
+++ Java-programming/Part-1/sandbox.java
@@ -1,6 +1,8 @@
+
 public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
+        System.out.println("There, I'm finally starting programming with a 'proper' programming language");
     }
 }
