c60f3cbb6edd0350a8e9434a5be6b658d8884b37
Tom <157364916+tomburger64@users.noreply.github.com>
2024-11-14 15:24:54 +0100

    print exercise 1.java

---
+++ Java-programming/Part-1/print-exercise-1.java
@@ -0,0 +1,10 @@
+// Add a new line below the line comment that prints the string "Ada Lovelace" and run the program. The output of the program should be Ada Lovelace
+
+
+public class AdaLovelace {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Ada Lovelace");
+    }
+}
