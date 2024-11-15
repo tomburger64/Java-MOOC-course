1ab0b0759fc4318e06b2e6b317ed6441fc41b44c
Tom <157364916+tomburger64@users.noreply.github.com>
2024-11-15 21:44:56 +0100

    print-exercise-3.java

---
+++ Java-programming/Part-1/print-exercise-3.java
@@ -0,0 +1,13 @@
+// Edit the program so that it will print the following text. Use the "sout"-shorthand for writing the print commands.
+// Once upon a time
+// there was
+// a dinosaur
+public class Dinosaur {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
+    }
+}
