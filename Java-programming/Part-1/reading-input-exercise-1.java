c29eca5fe069335a91fa354264490499666d56b1
Tom <157364916+tomburger64@users.noreply.github.com>
2024-11-27 14:09:48 +0100

    Create reading-input-exercise-1.java

---
+++ Java-programming/Part-1/reading-input-exercise-1.java
@@ -0,0 +1,15 @@
+/* Write a program that asks the user to write a string. When the user has provided a string (i.e., written some text and pressed the enter key), the program should print the string that was provided by the user.
+The exercise template comes with a program template that includes the creation of a Scanner tool.*/
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        // Write your program here
+
+    }
+}
