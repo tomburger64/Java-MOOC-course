480fbcb3aaf074fb801732de7d88a48cde89277d
Tom <157364916+tomburger64@users.noreply.github.com>
2024-11-27 14:21:37 +0100

    Create reading-input-exercise-2.java

---
+++ Java-programming/Part-1/reading-input-exercise-2.java
@@ -0,0 +1,13 @@
+/* Modify the program so that it prints the contents of the variable name, and the printed text is the following in its full form:
+Hi Ada Lovelace!
+NB! When using the System.out.println command, do not pass in the string "Ada Lovelace" as a parameter. Instead, use the existing variable name: System.out.println("Hi " + ...)
+*/
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+
+        System.out.println(name);
+    }
+}
