9877ad72d913af2ff417ef97e9a077f9be547832
Tom <157364916+tomburger64@users.noreply.github.com>
2024-11-28 14:37:49 +0100

    Create reading-input-exercise-5.java

---
+++ Java-programming/Part-1/reading-input-exercise-5.java
@@ -0,0 +1,25 @@
+/* Write a program that works as follows:
+
+Greetings! How are you doing?
+Good thank you!
+Oh, how interesting. Tell me more!
+Well, there's really nothing to tell.
+Thanks for sharing!
+
+Greetings! How are you doing?
+Nice and dandy like cotton candy!
+Oh, how interesting. Tell me more!
+Just went shopping.
+Thanks for sharing!*/
+
+import java.util.Scanner;
+
+public class Conversation {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+
+    }
+}
