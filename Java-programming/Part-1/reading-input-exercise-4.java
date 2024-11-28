00cfc0c52148fd148b5b0766ab2225d76b021fc1
Tom <157364916+tomburger64@users.noreply.github.com>
2024-11-28 14:29:28 +0100

    Create reading-input-exercise-4.java

---
+++ Java-programming/Part-1/reading-input-exercise-4.java
@@ -0,0 +1,15 @@
+/* Write a program that prompts the user for their name with the message "What's your name?". When the user has written their name, the program has to print "Hi " followed by the user's name.
+
+The exercise template already includes the code that creates the Scanner tool. */
+
+import java.util.Scanner;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+
+    }
+}
