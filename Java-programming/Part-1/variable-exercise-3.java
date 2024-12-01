1270af58f7c14e865c27e2475ce2a7acf5407430
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-01 15:58:20 +0100

    Create variable-exercise-3.java

---
+++ Java-programming/Part-1/variable-exercise-3.java
@@ -0,0 +1,23 @@
+/* Write a program that asks the user for a floating-point number using the variable type Double. The program then prints the user's input value.
+
+Example prints for the program can be seen below:
+Give a number:
+3.14
+You gave the number 3.14
+
+Give a number:
+2.718
+You gave the number 2.718 */
+
+
+import java.util.Scanner;
+
+public class DoubleInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // write your program here
+
+    }
+}
