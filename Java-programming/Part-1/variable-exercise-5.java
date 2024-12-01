d1624bf908745b3a8401104d8ef80306beca43a5
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-01 16:17:12 +0100

    Create variable-exercise-5.java

---
+++ Java-programming/Part-1/variable-exercise-5.java
@@ -0,0 +1,29 @@
+/* Write a program that asks the user for a string, an integer, a floating-point number, and a boolean. The program should then print the values given by the user.
+
+Example prints for the program can be seen below.
+
+Give a string:
+bye-bye
+Give an integer:
+11
+Give a double:
+4.2
+Give a boolean:
+true
+You gave the string bye-bye
+You gave the integer 11
+You gave the double 4.2
+You gave the boolean true */
+
+
+import java.util.Scanner;
+
+public class DifferentTypesOfInput {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here
+        
+    }
+}
