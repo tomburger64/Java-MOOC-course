64b09ebef485fa318d4db7d1e8a43fc099fd75aa
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-01 16:03:59 +0100

    Create variable-exercise-4.java

---
+++ Java-programming/Part-1/variable-exercise-4.java
@@ -0,0 +1,23 @@
+/* Write a program that asks the user for a boolean value. The program should then print the value provided by the user.
+
+Example prints for the program can be seen below.
+Write something:
+santa does not exist
+True or false? false
+
+Write something:
+TRUE
+True or false? true */
+
+
+import java.util.Scanner;
+
+public class BooleanInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // write your program here
+
+    }
+}
