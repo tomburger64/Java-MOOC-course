81de1942d5d73e1bd214f52e10d8cf9eb4c66da8
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-01 15:49:46 +0100

    Create variable-exercise-2.java

---
+++ Java-programming/Part-1/variable-exercise-2.java
@@ -0,0 +1,23 @@
+/* Write a program that asks the user for a value. The program should then print the value provided by the user.
+
+Here's a couple of examples:
+Give a number:
+3
+You gave the number 3
+
+Give a number:
+42
+You gave the number 42 */
+
+
+import java.util.Scanner;
+
+public class IntegerInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // write your program here
+
+    }
+}
