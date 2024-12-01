89c9513428feffe29ae70ef3a55d56a06d0ab43e
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-01 16:17:23 +0100

    variable-exercise-5.java done

--- Java-programming/Part-1/variable-exercise-5.java
+++ Java-programming/Part-1/variable-exercise-5.java
@@ -24,6 +24,21 @@ public class DifferentTypesOfInput {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a string:");
+        String stringVar = scan.nextLine();
         
+        System.out.println("Give an integer:");
+        int intVar = Integer.valueOf(scan.nextLine());
+        
+        System.out.println("Give a double:");
+        double doubleVar = Double.valueOf(scan.nextLine());
+        
+        System.out.println("Give a boolean:");
+        boolean boolVar = Boolean.valueOf(scan.nextLine());
+        
+        System.out.println("You gave the string " + stringVar);
+        System.out.println("You gave the integer " + intVar);
+        System.out.println("You gave the double " + doubleVar);
+        System.out.println("You gave the boolean " + boolVar);
     }
 }
