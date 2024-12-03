d4dc51bd3051c7419297a1d699e437caa2e4e944
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-03 14:59:01 +0100

    Create calc-w-nums-exercise-1.java

---
+++ Java-programming/Part-1/calc-w-nums-exercise-1.java
@@ -0,0 +1,35 @@
+/* In the exercise template, implement a program that asks the user for the number of days. After that, the program prints the number of seconds in the given number of days.
+
+Earlier we learned to read an integer in the following manner:
+
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("Give a number:");
+int number = Integer.valueOf(scanner.nextLine());
+System.out.println("You gave " + number);
+
+Examples of expected output:
+How many days would you like to convert to seconds?
+1
+86400
+
+How many days would you like to convert to seconds?
+3
+259200
+
+How many days would you like to convert to seconds?
+7
+604800 */
+
+
+import java.util.Scanner;
+
+public class SecondsInADay {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+      
+    }
+}
