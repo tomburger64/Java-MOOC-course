d9bda6cfd4c2a09e7647b8de217c5eb3e1c50fce
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-03 15:59:41 +0100

    calc-w-nums-exercise-6.java done

--- Java-programming/Part-1/calc-w-nums-exercise-6.java
+++ Java-programming/Part-1/calc-w-nums-exercise-6.java
@@ -14,6 +14,13 @@ public class AverageOfTwoNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
         
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        
+        double result = (1.0 * firstNum + secondNum) / 2;
+        System.out.println("The average is " + (result));
     }
 }
