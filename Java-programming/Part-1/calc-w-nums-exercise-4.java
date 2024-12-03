6ccbc7baff381cb33aff34fe982f05f69033716f
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-03 15:35:42 +0100

    calc-w-nums-exercise-4.java done

--- Java-programming/Part-1/calc-w-nums-exercise-4.java
+++ Java-programming/Part-1/calc-w-nums-exercise-4.java
@@ -22,6 +22,12 @@ public class AdditionFormula {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
-
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println(firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
     }
 }
