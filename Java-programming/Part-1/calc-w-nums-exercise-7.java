6aecd2229bbc061668742e059cd1a378fb2a0ba4
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-03 16:05:24 +0100

    calc-w-nums-exercise-7.java done

--- Java-programming/Part-1/calc-w-nums-exercise-7.java
+++ Java-programming/Part-1/calc-w-nums-exercise-7.java
@@ -25,6 +25,16 @@ public class AverageOfThreeNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the third number:");
+        int thirdNum = Integer.valueOf(scanner.nextLine());
+        
+        double result = (1.0 * firstNum + secondNum + thirdNum) / 3;
+        System.out.println("The average is " + result);
     }
 }
