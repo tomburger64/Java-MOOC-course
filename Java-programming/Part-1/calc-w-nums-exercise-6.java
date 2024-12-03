c8228eceb3c25f6a564c0f450e74aa3c74d0363e
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-03 15:51:27 +0100

    Create calc-w-nums-exercise-6.java

---
+++ Java-programming/Part-1/calc-w-nums-exercise-6.java
@@ -0,0 +1,19 @@
+/* Write a program that asks the user for two integers and prints their average.
+Give the first number:
+8
+Give the second number:
+2
+The average is 5.0 */
+
+
+import java.util.Scanner;
+
+public class AverageOfTwoNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        
+    }
+}
