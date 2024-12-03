afdfdf7f142d547ce2934394c3fbe5e09ac9e482
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-03 15:24:04 +0100

    calc-w-nums-exercise-2.java done

--- Java-programming/Part-1/calc-w-nums-exercise-2.java
+++ Java-programming/Part-1/calc-w-nums-exercise-2.java
@@ -32,6 +32,12 @@ public class SumOfTwoNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("The sum of the numbers is " + (firstNum + secondNum));
     }
 }
