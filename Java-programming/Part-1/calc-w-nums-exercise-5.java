20e54f3a700f4fdcb269c827ae111858dde3f830
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-03 15:37:28 +0100

    Create calc-w-nums-exercise-5.java

---
+++ Java-programming/Part-1/calc-w-nums-exercise-5.java
@@ -0,0 +1,28 @@
+/* Similar to the previous exercise, create a program that multiplies the values stored in two integer variables.
+
+For instance, if the entered numbers are 2 and 8, the program should print the following:
+Give the first number:
+2
+Give the second number:
+8
+2 * 8 = 16
+
+Likewise, if the entered numbers are 277 and 111, the print should be the following:
+Give the first number:
+277
+Give the second number:
+111
+277 * 111 = 30747 */
+
+
+import java.util.Scanner;
+
+public class MultiplicationFormula {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+
+    }
+}
