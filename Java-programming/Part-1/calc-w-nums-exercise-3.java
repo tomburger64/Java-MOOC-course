d349842aca731966614ed53c5a4c768858a38026
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-03 15:30:14 +0100

    calc-w-nums-exercise-3.java done

--- Java-programming/Part-1/calc-w-nums-exercise-3.java
+++ Java-programming/Part-1/calc-w-nums-exercise-3.java
@@ -26,6 +26,15 @@ public class SumOfThreeNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
         
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the third number:");
+        int thirdNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("The sum of the numbers is " + (firstNum + secondNum + thirdNum));
     }
 }
