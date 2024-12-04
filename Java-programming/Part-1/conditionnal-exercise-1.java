848ebb9d02b6d98623246e8398eed9e6c01be4db
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-04 16:02:05 +0100

    Create conditionnal-exercise-1.java

---
+++ Java-programming/Part-1/conditionnal-exercise-1.java
@@ -0,0 +1,20 @@
+/* Write a program that asks the user for an integer and prints the string "Speeding ticket!" if the input is greater than 120.
+
+Give speed:
+15
+
+Give speed:
+135
+Speeding ticket! */
+
+
+import java.util.Scanner;
+
+public class SpeedingTicket {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here. 
+    }
+}
