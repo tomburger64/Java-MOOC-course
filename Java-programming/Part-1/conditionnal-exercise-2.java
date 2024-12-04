9b1c4159a31a8ef65494cf31068a8e84efab4d17
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-04 22:13:38 +0100

    conditionnal-exercise-2.java done

--- Java-programming/Part-1/conditionnal-exercise-2.java
+++ Java-programming/Part-1/conditionnal-exercise-2.java
@@ -5,7 +5,8 @@ Try to run the tests before doing anything. TMC shows the indentation errors dif
 When you notice how indentation errors are shown, correct them. Now would be a good time to give the automatic code formatting a try. */
 
 
-import java.util.Scanner;
+// leaving original template so you don't have to go through the commit history, with indentation errors :
+/* import java.util.Scanner;
 
 public class CheckYourIndentation {
 
@@ -21,6 +22,34 @@ else if (first > second) { System.out.println("The first was larger than the sec
 else {
 System.out.println("The second was larger than the first!");
 }
+        
+
+    }
+} */
+
+
+// my solution
+import java.util.Scanner;
+
+public class CheckYourIndentation {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give a number: ");
+        int first = Integer.valueOf(scan.nextLine());
+        System.out.println("Give another number: ");
+        int second = Integer.valueOf(scan.nextLine());
+        if (first == second) {
+            System.out.println("Same!");
+        } else if (first > second) {
+            System.out.println("The first was larger than the second!");
+        } else {
+            System.out.println("The second was larger than the first!");
+        }
+        // if I hadn't seen the example of "fusing" the if statements ending with the following "else if" / "else"a few times online, I couldn't have known that without cheating
+        // that wasn't in the documentation / given examples at all
 
     }
 }
+
