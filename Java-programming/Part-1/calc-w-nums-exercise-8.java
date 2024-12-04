2accbc5b906ff1e1bc66d1131f03cf5b11c387a9
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-04 15:55:06 +0100

    calc-w-nums-exercise-8.java done

--- Java-programming/Part-1/calc-w-nums-exercise-8.java
+++ Java-programming/Part-1/calc-w-nums-exercise-8.java
@@ -26,6 +26,18 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        
+        // appart var to have double value result
+        double divideResult = 1.0 * firstNum / secondNum;
+        
+        System.out.println(firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
+        System.out.println(firstNum + " - " + secondNum + " = " + (firstNum - secondNum));
+        System.out.println(firstNum + " * " + secondNum + " = " + (firstNum * secondNum));
+        System.out.println(firstNum + " / " + secondNum + " = " + divideResult);
     }
 }
