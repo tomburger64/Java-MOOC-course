ff1d79619f1e3d4c462d08c6897325e53b11299d
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 15:54:29 +0100

    Create conditionnal-exercise-11.java

---
+++ Java-programming/Part-1/conditionnal-exercise-11.java
@@ -0,0 +1,25 @@
+/* Write a program that prompts the user for two strings. If the strings are the same, then the program prints "Same". Otherwise, it prints "Different".
+
+Enter the first string:
+hello
+Enter the second string:
+hello
+Same
+
+Enter the first string:
+hello
+Enter the second string:
+world
+Different */
+
+
+import java.util.Scanner;
+
+public class Same {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here. 
+    }
+}
