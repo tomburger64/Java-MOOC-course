92505029fcdb6bd9aeab841d2b1a104a929bb1b7
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 15:38:49 +0100

    conditionnal-exercise-9.java done

--- Java-programming/Part-1/conditionnal-exercise-9.java
+++ Java-programming/Part-1/conditionnal-exercise-9.java
@@ -32,5 +32,14 @@ public class OddOrEven {
         // int luku = 8
         // System.out.println( luku%2 );
         // So, by taking the modulo of a number and two you can find out if it is even or odd !
+
+        System.out.println("Give a number:");
+        int num = Integer.valueOf(scan.nextLine());
+        
+        if (num % 2 == 0) {
+            System.out.println("Number " + num + " is even.");
+        } else {
+            System.out.println("Number " + num + " is odd.");
+        }
     }
 }
