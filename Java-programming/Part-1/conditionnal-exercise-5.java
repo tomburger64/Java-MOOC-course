3e2b18c7dd0d94b2fb03ba1194b4d7d95b69a046
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 14:27:38 +0100

    Create conditionnal-exercise-5.java

---
+++ Java-programming/Part-1/conditionnal-exercise-5.java
@@ -0,0 +1,21 @@
+/* Write a program that prompts the user for an integer and informs the user whether or not it is positive (greater than zero).
+
+Give a number:
+5
+The number is positive.
+
+Give a number:
+-2
+The number is not positive. */
+
+
+import java.util.Scanner;
+
+public class Positivity {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here
+    }
+}
