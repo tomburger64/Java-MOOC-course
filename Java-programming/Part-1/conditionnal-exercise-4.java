f1cbb6ae0ac93e47afdf5ce020020470575686fd
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 14:22:20 +0100

    Create conditionnal-exercise-4.java

---
+++ Java-programming/Part-1/conditionnal-exercise-4.java
@@ -0,0 +1,20 @@
+/* Write a program that prompts the user for a year. If the user inputs a number that is smaller than 2015, then the program prints the string "Ancient history!".
+
+Give a year:
+2017
+
+Give a year:
+2013
+Ancient history! */
+
+
+import java.util.Scanner;
+
+public class Ancient {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here
+    }
+}
