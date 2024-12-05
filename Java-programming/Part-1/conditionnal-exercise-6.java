ecce61da39924862f4c095c0c509c9d51d0bf18a
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 14:35:40 +0100

    Create conditionnal-exercise-6.java

---
+++ Java-programming/Part-1/conditionnal-exercise-6.java
@@ -0,0 +1,21 @@
+/* Write a program that prompts the user for their age and tells them whether or not they are an adult (18 years old or older).
+
+How old are you?
+12
+You are not an adult
+
+How old are you?
+32
+You are an adult */
+
+
+import java.util.Scanner;
+
+public class Adulthood {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here 
+    }
+}
