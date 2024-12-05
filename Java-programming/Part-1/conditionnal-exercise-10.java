b86669dbdd288cb55d7945b77773a9d5ab6f6ed1
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 15:53:03 +0100

    conditionnal-exercise-10.java done

--- Java-programming/Part-1/conditionnal-exercise-10.java
+++ Java-programming/Part-1/conditionnal-exercise-10.java
@@ -17,5 +17,13 @@ public class Password {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here 
+        System.out.println("Password?");
+        String psw = scan.nextLine();
+        
+        if (psw.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
