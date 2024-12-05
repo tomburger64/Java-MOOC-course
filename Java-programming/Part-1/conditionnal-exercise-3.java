50598d6eeb33d3c0d8402e7df99cad62d506cfa5
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 14:17:35 +0100

    Create conditionnal-exercise-3.java

---
+++ Java-programming/Part-1/conditionnal-exercise-3.java
@@ -0,0 +1,20 @@
+/* Write a program that prompts the user for an integer and prints the string "Orwell" if the number is exactly 1984.
+
+Give a number:
+1983
+
+Give a number:
+1984
+Orwell */
+
+
+import java.util.Scanner;
+
+public class Orwell {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here
+    }
+}
