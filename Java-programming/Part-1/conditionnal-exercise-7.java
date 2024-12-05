fc97b76571140b32c61e23faafa201f8594b0cbe
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 14:46:29 +0100

    conditionnal-exercise-7.java done

--- Java-programming/Part-1/conditionnal-exercise-7.java
+++ Java-programming/Part-1/conditionnal-exercise-7.java
@@ -27,6 +27,21 @@ public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // notocie how they don't say "write your code here" anymore from this exercise
+        // also scanner seems to always be called scan and no longer scanner since a few exercises
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scan.nextLine());
         
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scan.nextLine());
+        
+        if (firstNum > secondNum) {
+            System.out.println("Greater number is:" + firstNum);
+        } else if (firstNum < secondNum) {
+            System.out.println("Greater number is:" + secondNum);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
     }
 }
