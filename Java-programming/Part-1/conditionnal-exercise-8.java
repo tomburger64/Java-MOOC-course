9d4f68ec0567f36c2ce9c098d1365969ab00b441
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 14:49:22 +0100

    Create conditionnal-exercise-8.java

---
+++ Java-programming/Part-1/conditionnal-exercise-8.java
@@ -0,0 +1,38 @@
+/* The table below describes how the grade for a particular course is determined. Write a program that gives a course grade according to the provided table.
+
+points	grade
+< 0	impossible!
+0-49	failed
+50-59	1
+60-69	2
+70-79	3
+80-89	4
+90-100	5
+> 100	incredible!
+
+Give points [0-100]:
+37
+Grade: failed
+
+Give points [0-100]:
+76
+Grade: 3
+
+Give points [0-100]:
+95
+Grade: 5
+
+Give points [0-100]:
+-3
+Grade: impossible! */
+
+
+import java.util.Scanner;
+
+public class GradesAndPoints {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
