5a64fbed47bf73508d523c9a734960549a7297bc
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 14:20:40 +0100

    conditionnal-exercise-3.java done

--- Java-programming/Part-1/conditionnal-exercise-3.java
+++ Java-programming/Part-1/conditionnal-exercise-3.java
@@ -16,5 +16,11 @@ public class Orwell {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a number:");
+        int num = Integer.valueOf(scan.nextLine());
+        
+        if (num == 1984) {
+            System.out.println("Orwell1");
+        }        
     }
 }
