aae404c30f7f4f990b13d0d33eed321aaa1cc1e1
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 14:40:17 +0100

    Create conditionnal-exercise-7.java

---
+++ Java-programming/Part-1/conditionnal-exercise-7.java
@@ -0,0 +1,32 @@
+/* Write a program that prompts the user for two integers and prints the larger of the two. If the numbers are the same, then the program informs us about this as well.
+
+Sample outputs:
+
+Give the first number:
+5
+Give the second number:
+3
+Greater number is: 5
+
+Give the first number:
+5
+Give the second number:
+8
+Greater number is: 8
+
+Give the first number:
+5
+Give the second number:
+5
+The numbers are equal! */
+
+
+import java.util.Scanner;
+
+public class LargerThanOrEqualTo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
