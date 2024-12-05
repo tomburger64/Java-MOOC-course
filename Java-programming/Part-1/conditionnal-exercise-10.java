71fbc32ecda89f9a28833e1302dbb1aee40086d3
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-05 15:49:29 +0100

    Create conditionnal-exercise-10.java

---
+++ Java-programming/Part-1/conditionnal-exercise-10.java
@@ -0,0 +1,21 @@
+/* Write a program that prompts the user for a password. If the password is "Caput Draconis" the program prints "Welcome!". Otherwise, the program prints "Off with you!"
+
+Password?
+Wattlebird
+Off with you!
+
+Password?
+Caput Draconis
+Welcome! */
+
+
+import java.util.Scanner;
+
+public class Password {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here 
+    }
+}
