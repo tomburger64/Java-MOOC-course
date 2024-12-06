dc61a344bdc389e1ac1b94b119bd09b6e9bbdadf
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-06 15:28:52 +0100

    Create conditionnal-exercise-13.java

---
+++ Java-programming/Part-1/conditionnal-exercise-13.java
@@ -0,0 +1,26 @@
+/* A year is a leap year if it is divisible by 4. However, if the year is divisible by 100, then it is a leap year only when it is also divisible by 400.
+
+Write a program that reads a year from the user, and checks whether or not it is a leap year.
+
+Give a year: 2011
+The year is not a leap year.
+
+Give a year: 2012
+The year is a leap year.
+
+Give a year: 1800
+The year is not a leap year.
+
+Give a year: 2000
+The year is a leap year. */
+
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
