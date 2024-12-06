aba99ee4b006b386f05f9a8aab157b19afa4402d
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-06 15:29:35 +0100

    Update conditionnal-exercise-13.java

--- Java-programming/Part-1/conditionnal-exercise-13.java
+++ Java-programming/Part-1/conditionnal-exercise-13.java
@@ -12,7 +12,11 @@ Give a year: 1800
 The year is not a leap year.
 
 Give a year: 2000
-The year is a leap year. */
+The year is a leap year.
+
+Hint 1: The divisibility by a particular number can be checked using the modulo operator, aka %, in the following way.
+
+Hint 2: Think of the problem as a chain of if, else if, else if, ... comparisons, and start building the program from a situation in which you can be certain that the year is not a leap year. */
 
 
 import java.util.Scanner;
