c3dc6af05ba36f2b6468116253551daed2760fe3
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-06 14:58:45 +0100

    Create conditionnal-exercise-12.java

---
+++ Java-programming/Part-1/conditionnal-exercise-12.java
@@ -0,0 +1,21 @@
+/* Write a program that prompts the user to input their age and checks whether or not it is possible (at least 0 and at most 120). Only use a single if-command in your program.
+
+How old are you? 10
+OK
+
+How old are you? 55
+OK
+
+How old are you? 150
+Impossible! */
+
+
+import java.util.Scanner;
+
+public class CheckingTheAge {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
