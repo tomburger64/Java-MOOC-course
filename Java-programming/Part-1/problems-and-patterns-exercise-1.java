fd855f942c5ba6c1ab4d3e57999fab51ec25c2e1
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-08 13:40:21 +0100

    problems-and-patterns-exercise-1.java done

--- Java-programming/Part-1/problems-and-patterns-exercise-1.java
+++ Java-programming/Part-1/problems-and-patterns-exercise-1.java
@@ -8,5 +8,8 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int value = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println(value * value);
     }
 }
