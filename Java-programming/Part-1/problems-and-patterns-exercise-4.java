cfcded9ae626cf211c867500abd2c968271541d0
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-09 15:45:28 +0100

    problems-and-patterns-exercise-4.java done

--- Java-programming/Part-1/problems-and-patterns-exercise-4.java
+++ Java-programming/Part-1/problems-and-patterns-exercise-4.java
@@ -22,5 +22,15 @@ public class ComparingNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int value1 = Integer.valueOf(scanner.nextLine());
+        int value2 = Integer.valueOf(scanner.nextLine());
+        
+        if (value1 > value2) {
+            System.out.println(value1 + " is greater than " + value2 + ".");
+        } else if (value1 < value2) {
+            System.out.println(value1 + " is smaller than " + value2 + ".");
+        } else {
+            System.out.println(value1 + " is equal to " + value2 + ".");
+        }
     }
 }
