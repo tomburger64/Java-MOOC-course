fe7e9666d2483057eb0641bf37b78a23a56ce8a0
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-09 15:52:35 +0100

    Create problems-and-patterns-exercise-5.java

---
+++ Java-programming/Part-1/problems-and-patterns-exercise-5.java
@@ -0,0 +1,21 @@
+/* Write a program by using the loop example that asks "Shall we carry on?" until the user inputs the string "no".
+
+Shall we carry on?
+yes
+Shall we carry on?
+ye
+Shall we carry on?
+y
+Shall we carry on?
+no */
+
+
+import java.util.Scanner;
+
+public class CarryOn {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
