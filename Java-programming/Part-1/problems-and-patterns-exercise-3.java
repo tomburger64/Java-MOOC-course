0b83f9df2477de698d34a0cc1b100671437781dc
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-09 15:37:32 +0100

    Create problems-and-patterns-exercise-3.java

---
+++ Java-programming/Part-1/problems-and-patterns-exercise-3.java
@@ -0,0 +1,21 @@
+/* Write a program that reads an integer from the user. If the number is less than 0, the program prints the given integer multiplied by -1. In all other cases, the program prints the number itself. A few examples of how the program's expected to function are shown below:
+
+-3
+3
+
+2
+2
+
+-5
+5   */
+
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
