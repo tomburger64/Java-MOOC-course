716d8a8d391db5c34b15e0972ecb309a7d28785e
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-09 16:09:17 +0100

    problems-and-patterns-exercise-5.java done

--- Java-programming/Part-1/problems-and-patterns-exercise-5.java
+++ Java-programming/Part-1/problems-and-patterns-exercise-5.java
@@ -17,5 +17,14 @@ public class CarryOn {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // initially made the mistake of having an if statement determine if the msg was = to "no" and "break;", and then (without an 'else') print the "shall we carry" thing
+        while (true) {
+            System.out.println("Shall we carry on?");
+            String input = scanner.nextLine();
+            
+            if (input.equals("no")) {
+                break;
+            }
+        }
     }
 }
