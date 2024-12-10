9c9603389ff1770af22679dadb4df44737c0f251
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-10 13:38:51 +0100

    repeating-exercise-2.java done

--- Java-programming/Part-1/repeating-exercise-2.java
+++ Java-programming/Part-1/repeating-exercise-2.java
@@ -18,6 +18,14 @@ public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            
+            if (num == 4) {
+                break;
+            }
+        }
     }
 }
