452309a45369f97ef811b0be7d04bf9a5e0ce5a3
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-11 16:00:27 +0100

    Create repeating-exercise-9.java

---
+++ Java-programming/Part-1/repeating-exercise-9.java
@@ -0,0 +1,36 @@
+/* Write a program that asks the user for input until the user inputs 0. After this, the program prints the average of the positive numbers (numbers that are greater than zero).
+
+If no positive number is inputted, the program prints "Cannot calculate the average"
+
+Below a few examples of the programs output
+
+3
+5
+1
+-3
+0
+3.0
+
+0
+Cannot calculate the average
+
+-3
+1
+0
+1.0
+
+1
+1
+0
+1.0 */
+
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
