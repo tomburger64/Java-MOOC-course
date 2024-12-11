1216b784f8a715a4a33b958133c87d6e29a4fcb8
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-11 15:29:36 +0100

    repeating-exercise-5.java done
    struggled cause I forgot to add the final log in case 0 is enterred

--- Java-programming/Part-1/repeating-exercise-5.java
+++ Java-programming/Part-1/repeating-exercise-5.java
@@ -22,5 +22,16 @@ public class NumberOfNegativeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            
+            if (num == 0) {
+                System.out.println("Number of negative numbers: " + howManyNeg);
+                break;
+            } else if (num < 0) {
+                howManyNeg = howManyNeg + 1;
+            }
+        }
     }
 }
