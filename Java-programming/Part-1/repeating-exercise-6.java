cb7ab327f34af9340f75a384a0673f6d0fd6b621
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-11 15:36:22 +0100

    repeating-exercise-6.java done

--- Java-programming/Part-1/repeating-exercise-6.java
+++ Java-programming/Part-1/repeating-exercise-6.java
@@ -22,5 +22,18 @@ public class SumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int numSum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            
+            if (num == 0) {
+                System.out.println("Sum of the numbers: " + numSum);
+                break;
+            } else {
+                numSum = numSum + num;
+                continue;
+            }
+        }
     }
 }
