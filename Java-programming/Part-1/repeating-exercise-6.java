caa64e9c403dbb1dc1c64d6f68354f3163f44298
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-11 15:32:25 +0100

    Create repeating-exercise-6.java

---
+++ Java-programming/Part-1/repeating-exercise-6.java
@@ -0,0 +1,26 @@
+/* Write a program that reads numbers from the user until the user inputs a number 0. After this the program outputs the sum of the numbers. The number zero does not need to be added to the sum, even if it does not change the results.
+
+Example output of the program
+
+Give a number:
+5
+Give a number:
+22
+Give a number:
+9
+Give a number:
+-2
+Give a number:
+0
+Sum of the numbers: 34 */
+
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
