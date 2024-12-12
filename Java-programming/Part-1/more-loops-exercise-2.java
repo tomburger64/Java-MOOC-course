02d7d8a28ec526cf533f65a7a858e393409f3493
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-12 15:42:53 +0100

    Create more-loops-exercise-2.java

---
+++ Java-programming/Part-1/more-loops-exercise-2.java
@@ -0,0 +1,29 @@
+/* Write a program, which reads an integer from the user. Then the program prints numbers from that number to 100. You can assume that the user always gives a number less than 100. Below are some examples of the expected functionality.
+
+99
+99
+100
+
+-4
+-4
+-3
+-2
+-1
+0
+1
+2
+... (many numbers in between) ...
+98
+99
+100 */
+
+
+import java.util.Scanner;
+
+public class CountingToHundred {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
