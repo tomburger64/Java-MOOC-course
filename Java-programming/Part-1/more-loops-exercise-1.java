b972bf7201637cdf473db349a47f9165a82b8475
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-12 15:37:38 +0100

    Create more-loops-exercise-1.java

---
+++ Java-programming/Part-1/more-loops-exercise-1.java
@@ -0,0 +1,19 @@
+/* Write a program that reads an integer from the user. Next, the program prints numbers from 0 to the number given by the user. You can assume that the user always gives a positive number. Below are some examples of the expected functionality.
+
+4
+0
+1
+2
+3
+4 */
+
+
+import java.util.Scanner;
+
+public class Counting {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
