c82e0888ae7ab6aec6c6fa6f268770061d303e7d
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-13 15:55:55 +0100

    Create more-loops-exercise-4.java

---
+++ Java-programming/Part-1/more-loops-exercise-4.java
@@ -0,0 +1,22 @@
+/* Implement a program, which calculates the sum 1+2+3+...+n where n is given as user input.
+
+Sample output:
+Last number? 3
+The sum is 6
+
+The previous example calculated 1 + 2 + 3 = 6
+Last number? 7
+The sum is 28
+
+And this one calculated 1 + 2 + 3 + 4 + 5 + 6 + 7 = 28 */
+
+
+import java.util.Scanner;
+
+public class SumOfASequence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
