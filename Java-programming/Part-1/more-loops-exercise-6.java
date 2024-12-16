82b766cafd65862d383c7881d9d94340fc1a9bdb
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-16 16:15:58 +0100

    more-loops-exercise-6.java done

--- Java-programming/Part-1/more-loops-exercise-6.java
+++ Java-programming/Part-1/more-loops-exercise-6.java
@@ -22,5 +22,19 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give a number:");
+        int num = Integer.valueOf(scanner.nextLine());
+        
+        // gives the factorial of the asked number
+        // also adding the asked number to the sum (loop doesn't do it by itself)
+        
+        // no default value should be at 0 (no (a * 0) + (b * 0) + ...)
+        int sum = 1;
+        for (int n = 1; n != num; n++) {
+            sum = sum * n;
+        }
+        sum = sum * num;
+        
+        System.out.println("Factorial: " + sum);
     }
 }
