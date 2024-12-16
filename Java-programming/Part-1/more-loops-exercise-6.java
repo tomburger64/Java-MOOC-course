37e1bdfb1eecf27dfa57533d936406dac4b6855e
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-16 16:15:35 +0100

    Create more-loops-exercise-6.java

---
+++ Java-programming/Part-1/more-loops-exercise-6.java
@@ -0,0 +1,26 @@
+/* Implement a program which calculates the factorial of a number given by the user.
+
+Factorial of n, denoted n!, is calculated as 1 * 2 * 3 * ... * n. For example, the factorial of 4 is 24 or 4! = 1 * 2 * 3 * 4 = 24. Additionally, it has been specified that the factorial of 0 is 1, so 0! = 1.
+
+Sample output:
+
+Give a number: 3
+Factorial: 6
+
+The internal calculation here was 1 * 2 * 3 = 6
+
+Give a number: 10
+Factorial: 3628800
+
+And now the internal calculation was 1 * 2 * 3 * ... * 8 * 9 * 10 = 3628800 */
+
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
