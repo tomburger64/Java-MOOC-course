2fe69d1bba0a5ff2d22b1786f2cb8b0da604ea9b
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-19 14:44:04 +0100

    Create methods-exercise-4.java

---
+++ Java-programming/Part-1/methods-exercise-4.java
@@ -0,0 +1,31 @@
+/* Create the following method in the exercise template: public static void printFromNumberToOne(int number). It should print the numbers from the number passed as a parameter down to one. Two examples of the method's usage are given below.
+
+public static void main(String[] args) {
+    printFromNumberToOne(5);
+}
+
+5
+4
+3
+2
+1
+
+
+public static void main(String[] args) {
+    printFromNumberToOne(2);
+}
+
+
+2
+1 */
+
+
+
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
