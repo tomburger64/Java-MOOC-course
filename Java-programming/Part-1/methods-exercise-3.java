7d1f37f508a05d7915c5c5cbdf7cbd176de01262
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-19 14:42:42 +0100

    methods-exercise-3.java done

--- Java-programming/Part-1/methods-exercise-3.java
+++ Java-programming/Part-1/methods-exercise-3.java
@@ -22,7 +22,13 @@ public static void main(String[] args) {
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        printUntilNumber(3);
+    }
+    
+    public static void printUntilNumber(int number) {
+        for (int i = 0; i != number; i++) {
+            System.out.println(i + 1);
+        }
     }
 
 }
