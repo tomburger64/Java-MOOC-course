4185e426af2d48a75e3d5f22c571766f92e201f3
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-19 14:19:04 +0100

    Create methods-exercise-1.java

---
+++ Java-programming/Part-1/methods-exercise-1.java
@@ -0,0 +1,24 @@
+/* Create a method called printText which prints the phrase "In a hole in the ground there lived a method" and a newline.
+
+public static void main(String[] args) {
+    printText();
+}
+
+public static void printText() {
+    // Write some code in here
+}
+
+The output of the program:
+
+In a hole in the ground there lived a method */
+
+public class InAHoleInTheGround {
+
+    public static void main(String[] args) {
+        printText();
+    }
+    
+    public static void printText() {
+        // Write some code in here
+    }
+}
