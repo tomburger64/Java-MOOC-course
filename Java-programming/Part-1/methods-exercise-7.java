2d36b9fdaafee9c3296fe4360a21bb450b78826b
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-21 15:00:48 +0100

    Create methods-exercise-7.java

---
+++ Java-programming/Part-1/methods-exercise-7.java
@@ -0,0 +1,33 @@
+/* Write a method public static void divisibleByThreeInRange(int beginning, int end) that prints all the numbers divisible by three in the given range. The numbers are to be printed in order from the smallest to the greatest.
+
+public static void main(String[] args) {
+    divisibleByThreeInRange(3, 6);
+}
+
+Sample output
+
+3
+6
+
+
+public static void main(String[] args) {
+    divisibleByThreeInRange(2, 10);
+}
+
+Sample output
+
+3
+6
+9 */
+
+
+import java.util.Scanner;
+
+public class DivisibleByThree {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+}
