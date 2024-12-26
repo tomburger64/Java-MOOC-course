2355532e4f4a4a8df00940627174bbfb816ed7de
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-26 15:33:43 +0100

    Create methods-exercise-11.java

---
+++ Java-programming/Part-1/methods-exercise-11.java
@@ -0,0 +1,33 @@
+/* Define a two-parameter method smallest that returns the smaller of the two numbers passed to it as parameters.
+
+public static int smallest(int number1, int number2) {
+    // write your code here
+    // do not print anything inside the method
+
+    // there must be a return command at the end
+}
+
+public static void main(String[] args) {
+    int answer =  smallest(2, 7);
+    System.out.println("Smallest: " + answer);
+}
+
+The output of the program:
+
+Smallest: 2 */
+
+
+public class Smallest {
+
+    public static int smallest(int number1, int number2) {
+        // Write some code here
+        // don't print anything inside this method
+        // there must be a return in the end of the method
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int result = smallest(2, 7);
+        System.out.println("Smallest: " + result);
+    }
+}
