5635c354cd53b46d0befd28c4e6ee2331125cd3a
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-28 16:06:10 +0100

    part 1 methods-exercise-14.java done

--- Java-programming/Part-1/methods-exercise-14.java
+++ Java-programming/Part-1/methods-exercise-14.java
@@ -37,7 +37,6 @@ public class StarSign {
         //The tests are not checking the main, so you can modify it freely.
         //NB: If the tests don't seem to pass, you should try the methods here
         //in the main to make sure they print the correct shapes!
-        
         printStars(3);
         System.out.println("\n---");  // printing --- between the shapes
         printSquare(4);
@@ -50,6 +49,9 @@ public class StarSign {
 
     public static void printStars(int number) {
         // first part of the exercise
+        for (int n = 0; n < number; n++) {
+            System.out.println("*");
+        }
     }
 
     public static void printSquare(int size) {
