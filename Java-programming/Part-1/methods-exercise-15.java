cad426197c8fd288b286cdd0ff257aea82f1fece
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-30 15:46:37 +0100

    Update methods-exercise-15.java

--- Java-programming/Part-1/methods-exercise-15.java
+++ Java-programming/Part-1/methods-exercise-15.java
@@ -3,7 +3,17 @@ Part 1: Printing stars and spaces
 
 Define a method called printSpaces(int number) that produces the number of spaces specified by number. The method does not print the line break.
 
-You will also have to either copy the printStars method from your previous exercise or reimplement it in this exercise template. */
+You will also have to either copy the printStars method from your previous exercise or reimplement it in this exercise template.
+
+
+Part 2: Printing a right-leaning triangle
+
+Create a method called printTriangle(int size) that uses printSpaces and printStars to print the correct triangle. So the method call printTriangle(4) should print the following:
+
+   *
+  **
+ ***
+**** */
 
 
 public class AdvancedAstrology {
