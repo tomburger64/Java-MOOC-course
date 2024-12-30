31856db681b7bc16df62a743c2889c46ff2a495a
Tom <157364916+tomburger64@users.noreply.github.com>
2024-12-30 15:33:41 +0100

    Create methods-exercise-15.java

---
+++ Java-programming/Part-1/methods-exercise-15.java
@@ -0,0 +1,36 @@
+/* NB! By submitting a solution to a part of an exercise which has multiple parts, you can get part of the exercise points. You can submit a part by using the 'submit' button on NetBeans. More on the programming exercise submission instructions: Exercise submission instructions.
+Part 1: Printing stars and spaces
+
+Define a method called printSpaces(int number) that produces the number of spaces specified by number. The method does not print the line break.
+
+You will also have to either copy the printStars method from your previous exercise or reimplement it in this exercise template. */
+
+
+public class AdvancedAstrology {
+
+    public static void printStars(int number) {
+        // part 1 of the exercise
+    }
+
+    public static void printSpaces(int number) {
+        // part 1 of the exercise
+    }
+
+    public static void printTriangle(int size) {
+        // part 2 of the exercise
+    }
+
+    public static void christmasTree(int height) {
+        // part 3 of the exercise
+    }
+
+    public static void main(String[] args) {
+        // The tests are not checking the main, so you can modify it freely.
+
+        printTriangle(5);
+        System.out.println("---");
+        christmasTree(4);
+        System.out.println("---");
+        christmasTree(10);
+    }
+}
