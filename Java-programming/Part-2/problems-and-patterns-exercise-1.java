70d4c1cc62c6078442148c9335223fb8769e1ba0
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-02 14:17:35 +0100

    Create problems-and-patterns-exercise-1.java

---
+++ Java-programming/Part-2/problems-and-patterns-exercise-1.java
@@ -0,0 +1,15 @@
+/* Write a program that reads an integer from the user and prints the square of the given integer, i.e. the integer multiplied by itself. */
+
+
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int value = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println(value * value);
+    }
+}
