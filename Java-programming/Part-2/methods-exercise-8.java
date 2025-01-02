66c2f58ee7276f421f7c245de9b36325813d5e1e
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-02 14:30:42 +0100

    Create methods-exercise-8.java

---
+++ Java-programming/Part-2/methods-exercise-8.java
@@ -0,0 +1,17 @@
+/* Write a method public static int numberUno() that returns the value 1. */
+
+
+import java.util.Scanner;
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+    
+    public static int numberUno() {
+        return 1;
+    }
+
+}
