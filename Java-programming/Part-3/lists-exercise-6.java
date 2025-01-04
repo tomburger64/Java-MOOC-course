4509c38be5293a5d4781bd67b18776b280cb550d
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-04 15:52:30 +0100

    Create lists-exercise-6.java

---
+++ Java-programming/Part-3/lists-exercise-6.java
@@ -0,0 +1,50 @@
+/* In the exercise template there is a program that reads inputs from the user and adds them to a list. Reading is stopped once the user enters an empty string.
+
+Modify the program to print both the first and the last values after the reading ends. You may suppose that at least two values are read into the list.
+
+Tom
+Emma
+Alex
+Mary
+
+Tom
+Mary
+
+
+
+Juno
+Elizabeth
+Mason
+Irene
+Olivia
+Liam
+Ida
+Christopher
+Mark
+Sylvester
+Oscar
+
+Juno
+Oscar */
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FirstAndLast {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+    }
+}
