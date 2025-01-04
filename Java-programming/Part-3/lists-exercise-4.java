52cbe76530057095f6cdf9bc6d457ee6fcaa2b08
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-04 15:37:57 +0100

    Create lists-exercise-4.java

---
+++ Java-programming/Part-3/lists-exercise-4.java
@@ -0,0 +1,48 @@
+/* In the exercise template is a program that reads input from the user. Modify its working so that when the program quits reading, the program prints the number of values on the list.
+
+Tom
+Emma
+Alex
+Mary
+
+In total: 4
+
+
+
+Juno
+Elizabeth
+Mason
+Irene
+Olivia
+Liam
+Ida
+Christopher
+Mark
+Sylvester
+Oscar
+
+In total: 11
+
+NB! Be sure to use the size method of the list. */
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListSize {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+    }
+}
