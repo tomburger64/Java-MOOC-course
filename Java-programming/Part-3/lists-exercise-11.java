00a92d185f1b660b346de9e778d2f84fe27b5414
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-05 13:49:31 +0100

    Create lists-exercise-11.java

---
+++ Java-programming/Part-3/lists-exercise-11.java
@@ -0,0 +1,46 @@
+/* Write a program that reads numbers from the user. When number 9999 is entered, the reading process stops. After this the program will print the smallest number in the list, and also the indices where that number is found. Notice: the smallest number can appear multiple times in the list.
+
+72
+2
+8
+8
+11
+9999
+
+Smallest number: 2
+Found at index: 1
+
+
+
+72
+44
+8
+8
+11
+9999
+
+Smallest number: 8
+Found at index: 2
+Found at index: 3
+
+Hint: combine the programs you wrote for the exercises "Greatest number in the list" and "Index of the requested number". First find the smallest number, and then find the index of that number. */
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOfSmallest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // implement here a program that reads user input
+        // until the user enters 9999
+        
+        // after that, the program prints the smallest number
+        // and its index -- the smallest number
+        // might appear multiple times
+
+        
+    }
+}
