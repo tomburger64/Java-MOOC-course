5ddb1e667897d815d591bada9b8808966bc89655
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-06 12:47:54 +0100

    Create lists-exercise-13.java

---
+++ Java-programming/Part-3/lists-exercise-13.java
@@ -0,0 +1,28 @@
+/* The exercise template contains a base that reads numbers from the user and adds them to a list. Reading is stopped once the user enters the number -1.
+
+When reading ends, calculate the average of the numbers in it, and then print that value.
+
+72
+2
+8
+11
+-1
+
+Average: 23.25 */
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfAList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // implement here a program, that first reads user input
+        // adding them on a list until user gives -1.
+        // Then it computes the average of the numbers on the list
+        // and prints it.
+        
+    }
+}
