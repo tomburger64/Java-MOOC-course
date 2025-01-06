5b3fc8a7bcdc5a3604495989f5f5ee330762acce
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-06 15:24:13 +0100

    Create lists-exercise-17.java

---
+++ Java-programming/Part-3/lists-exercise-17.java
@@ -0,0 +1,30 @@
+/* Create the method public static void removeLast(ArrayList<String> strings) in the exercise template. The method should remove the last value in the list it receives as a parameter. If the list is empty, the method does nothing.
+
+ArrayList<String> strings = new ArrayList<>();
+
+strings.add("First");
+strings.add("Second");
+strings.add("Third");
+
+System.out.println(strings);
+
+removeLast(strings);
+removeLast(strings);
+
+System.out.println(strings);
+
+
+
+[First, Second, Third]
+[First] */
+
+
+import java.util.ArrayList;
+
+public class RemoveLast {
+
+    public static void main(String[] args) {
+        // Try your method in here
+    }
+
+}
