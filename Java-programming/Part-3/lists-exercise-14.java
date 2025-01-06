ae3a37b7f9862712ad40f9fc7d7113d55bba413d
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-06 14:47:27 +0100

    Create lists-exercise-14.java

---
+++ Java-programming/Part-3/lists-exercise-14.java
@@ -0,0 +1,41 @@
+/* In the exercise template there is a program that reads inputs from the user until an empty string is entered. Add the following functionality to it: after reading the inputs one more string is requested from the user. The program then tell whether that string was found in the list or not.
+
+Tom
+Emma
+Alex
+Mary
+
+Search for? Mary
+Mary was found!
+
+
+
+Tom
+Emma
+Alex
+Mary
+
+Search for? Logan
+Logan was not found! */
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OnTheList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+    }
+}
