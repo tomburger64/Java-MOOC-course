022b7f24619faa4adf8c2ab1bffc5c00ee45763c
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-09 15:21:48 +0100

    Create arrays-exercise-5.java

---
+++ Java-programming/Part-3/arrays-exercise-5.java
@@ -0,0 +1,29 @@
+/* Complete the method public static void printArrayInStars(int[] array) in the class named 'Printer' to make it print a row of stars for each number in the array. The amount of stars on each row is defined by the corresponding number in the array.
+
+You can try out the printing with this example:
+
+int[] array = {5, 1, 3, 4, 2};
+printArrayInStars(array);
+
+*****
+*
+***
+****
+**
+
+The 0th element of the array is 5, so the first line has 5 stars. The next one has 1 etc. */
+
+
+public class Printer {
+
+    public static void main(String[] args) {
+        // You can test the method here
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+
+    public static void printArrayInStars(int[] array) {
+        // Write some code in here
+    }
+
+}
