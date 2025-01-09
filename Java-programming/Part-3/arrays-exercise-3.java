f06517a64abeb33743cab09cca44f06f5b97067f
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-09 15:03:46 +0100

    Create arrays-exercise-3.java

---
+++ Java-programming/Part-3/arrays-exercise-3.java
@@ -0,0 +1,23 @@
+/* The class SumOfArray has a method public static int sumOfNumbersInArray(int[] array). Complete the method so that it computes and returns the sum of the numbers in the array it receives as parameter.
+
+You can try out the computation of the sum with this example:
+
+int[] numbers = {5, 1, 3, 4, 2};
+sumOfNumbersInArray(numbers);
+
+15 */
+
+
+public class SumOfArray {
+
+    public static void main(String[] args) {
+        // You can try the method here
+        int[] array = {5, 1, 3, 4, 2};
+        System.out.println(sumOfNumbersInArray(array));
+    }
+
+    public static int sumOfNumbersInArray(int[] array) {
+        // Write some code here
+        return 0;
+    }
+}
