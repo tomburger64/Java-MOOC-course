d95025b9b8725651a13f5d12538923d04059cb6e
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-10 15:16:57 +0100

    Create using-strings-exercise-4.java

---
+++ Java-programming/Part-3/using-strings-exercise-4.java
@@ -0,0 +1,25 @@
+/* Write a program that reads strings from the user. If the input is empty, the program stops reading input and halts. For each non-empty input it splits the string input by whitespaces  and prints each part of the string on a new line.
+
+once upon a time
+once
+upon
+a
+time
+a little program
+a
+little
+program
+halted
+halted */
+
+
+import java.util.Scanner;
+
+public class LineByLine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
