c56f0fcce04ae9fed6c98e9981e911f348b91f97
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-10 14:56:42 +0100

    Create using-strings-exercise-2.java

---
+++ Java-programming/Part-3/using-strings-exercise-2.java
@@ -0,0 +1,20 @@
+/* Write a program that asks the user for a string. If the user writes the string "true", the program prints "You got it right!", otherwise it prints "Try again!".
+
+Give a string: true
+You got it right!
+
+
+
+Give a string: trueish
+Try again! */
+
+
+import java.util.Scanner;
+
+public class IsItTrue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
