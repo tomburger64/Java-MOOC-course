41a66df5d60a3076f2c87db51c9a511a52fce82a
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-10 14:58:59 +0100

    Update using-strings-exercise-2.java

--- Java-programming/Part-3/using-strings-exercise-2.java
+++ Java-programming/Part-3/using-strings-exercise-2.java
@@ -15,6 +15,14 @@ public class IsItTrue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Give a string: ");
+        String input = scanner.nextLine();
+        
+        if (input.equals("true")) {
+            System.out.println("You got it right!");
+        } else {
+            System.out.println("Try again!");
+        }
     }
 }
