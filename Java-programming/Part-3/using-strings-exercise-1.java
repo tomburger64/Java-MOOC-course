922e3eee1bdffb253646ac0feef367294a58d5b9
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-10 14:49:01 +0100

    Create using-strings-exercise-1.java

---
+++ Java-programming/Part-3/using-strings-exercise-1.java
@@ -0,0 +1,21 @@
+/* Write a program, that reads a string from the user and then prints it three times.
+Sample output
+
+Give a word: cake
+
+cakecakecake
+
+NB! The program should ask for only one string. Don't use a loop here. */
+
+
+import java.util.Scanner;
+
+public class PrintThrice {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+
+    }
+}
