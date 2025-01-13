7cdf466dc49649a9396705ff2d5693cd529a8944
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-13 15:53:31 +0100

    Create using-strings-exercise-6.java

---
+++ Java-programming/Part-3/using-strings-exercise-6.java
@@ -0,0 +1,19 @@
+/* Write a program that reads user input until an empty line is entered. For each non-empty line the program splits the string by spaces  and prints the first part of the string.
+Sample output
+
+one two three four
+one
+this is a very important message
+this */
+
+
+import java.util.Scanner;
+
+public class FirstWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
