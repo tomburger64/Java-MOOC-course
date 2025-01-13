fe988044b3fcd63aca1dec83e9ba3302b02fdb92
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-13 15:56:24 +0100

    Update using-strings-exercise-6.java

--- Java-programming/Part-3/using-strings-exercise-6.java
+++ Java-programming/Part-3/using-strings-exercise-6.java
@@ -14,6 +14,14 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            } else {
+                String[] inputs = input.split(" ");
+                System.out.println(inputs[0]);
+            }
+        }
     }
 }
