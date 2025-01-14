d4af022886561d1b0f2ad5af300c5cf7c3582311
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-14 14:52:25 +0100

    Update using-strings-exercise-7.java

--- Java-programming/Part-3/using-strings-exercise-7.java
+++ Java-programming/Part-3/using-strings-exercise-7.java
@@ -20,6 +20,15 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while (true) {
+            String input = scanner.nextLine();
+            String[] inputs = input.split(" ");
+            
+            if (input.equals("")) {
+                break;
+            } else {
+                System.out.println(inputs[inputs.length - 1]);
+            }
+        }
     }
 }
