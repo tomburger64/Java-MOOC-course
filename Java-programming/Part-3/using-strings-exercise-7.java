27fcb9c02bd8be63c9778734e6ad87df87860063
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-14 14:48:29 +0100

    Create using-strings-exercise-7.java

---
+++ Java-programming/Part-3/using-strings-exercise-7.java
@@ -0,0 +1,25 @@
+/* Write a program that reads user input until an empty line is entered. For each non-empty line the program splits the string by spaces  and prints the last part of the string.
+
+one two three four
+four
+this is a very important message
+message
+
+Tip! You can find out the length of the array like this:
+
+String[] parts = {"one", "two", "three"};
+System.out.println("Number of parts: " + parts.length);
+
+Number of parts: 3 */
+
+
+import java.util.Scanner;
+
+public class LastWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
