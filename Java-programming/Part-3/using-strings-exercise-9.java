e1542173ef298555ba4ae1611f021e7885877e09
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-15 15:14:36 +0100

    Create using-strings-exercise-9.java

---
+++ Java-programming/Part-3/using-strings-exercise-9.java
@@ -0,0 +1,23 @@
+/* Write a program that reads names and ages from the user until an empty line is entered. The name and age are separed by a comma.
+
+After reading all user input, the program prints the name of the oldest person. You can assume that the user enters at least one person, and the that one of the users is older than the others.
+
+sebastian,2
+lucas,2
+lily,1
+hanna,5
+gabriel,10
+
+Name of the oldest: gabriel */
+
+
+import java.util.Scanner;
+
+public class NameOfTheOldest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
