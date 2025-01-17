bc34050205c72b29366d93b72e57f5ebf37d6e17
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-17 16:01:03 +0100

    Update OOP-intro-ex-2.java

--- Java-programming/Part-4/OOP-intro-ex-2.java
+++ Java-programming/Part-4/OOP-intro-ex-2.java
@@ -6,8 +6,7 @@ Write a program that:
     Creates an account named "My account" with the balance 0
     Withdraws 100.0 from Matthew's account
     Deposits 100.0 to "my account"
-    Prints both the accounts
-*/
+    Prints both the accounts */
 
 
 public class YourFirstBankTransfer {
@@ -15,5 +14,14 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        
+        Account matthewsAccount = new Account("Matthews account", 1000.0);
+        Account myAccount = new Account("My account", 0.0);
+        
+        matthewsAccount.withdrawal(100.0);
+        myAccount.deposit(100.0);
+        
+        System.out.println(matthewsAccount);
+        System.out.println(myAccount);
     }
 }
