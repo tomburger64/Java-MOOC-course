d3494457db2de524f3fff89645bae91badc93714
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-17 15:51:51 +0100

    Update OOP-intro-ex-1.java

--- Java-programming/Part-4/OOP-intro-ex-1.java
+++ Java-programming/Part-4/OOP-intro-ex-1.java
@@ -24,5 +24,13 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        
+        /*Write a program that creates an account with a balance of 100.0,
+        deposits 20.0 in it, and finally prints the balance.
+        NB! Perform all the operations in this exact order.*/
+        
+        Account mrBeastAcc = new Account("MrBeast's account", 100.0);
+        mrBeastAcc.deposit(20.0);
+        System.out.println(mrBeastAcc);
     }
 }
