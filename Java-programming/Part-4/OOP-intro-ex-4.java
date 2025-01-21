d4191e588ca146abfeaf992deeff2f218ee739e4
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-21 16:03:47 +0100

    Update OOP-intro-ex-4.java

--- Java-programming/Part-4/OOP-intro-ex-4.java
+++ Java-programming/Part-4/OOP-intro-ex-4.java
@@ -9,16 +9,22 @@ Room
 
 This class doesn't do much either. However, in the following exercise the object instantiated from our class is already capable of printing text.*/
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-import java.util.Scanner;
-
-public class MainProgram {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Follow the instructions carefully 
-        // and run the tests.
+/**
+ *
+ * @author tomla
+ */
+public class Room {
+    private String code;
+    private int seats;
+    
+    public Room(String classCode, int numberOfSeats) {
+        this.code = classCode;
+        this.seats = numberOfSeats;
     }
-
 }
