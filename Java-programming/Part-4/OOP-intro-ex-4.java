9885e05e47de45ae0ed077d2350938ebadfbc649
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-21 15:46:55 +0100

    Create OOP-intro-ex-4.java

---
+++ Java-programming/Part-4/OOP-intro-ex-4.java
@@ -0,0 +1,24 @@
+/* Create a class named Room. Add the variables private String code and private int seats to the class. Then create a constructor public Room(String classCode, int numberOfSeats) through which values are assigned to the instance variables.
+
+Room
+---
+-code:String
+-numberOfSeats:int
+---
++Room(String,int)
+
+This class doesn't do much either. However, in the following exercise the object instantiated from our class is already capable of printing text.*/
+
+
+import java.util.Scanner;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Follow the instructions carefully 
+        // and run the tests.
+    }
+
+}
