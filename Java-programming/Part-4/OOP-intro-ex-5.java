83cd49375fe84f14f1ebfe58445cd39c9e54da26
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-22 15:59:14 +0100

    Create OOP-intro-ex-5.java

---
+++ Java-programming/Part-4/OOP-intro-ex-5.java
@@ -0,0 +1,32 @@
+/* Create a class named Whistle. Add the variable private String sound to the class. After that, create the constructor public Whistle(String whistleSound), which is used to create a new whistle that's given a sound.
+
+Then create the method public void sound() that prints the whistle's sound.
+
+Whistle duckWhistle = new Whistle("Kvaak");
+Whistle roosterWhistle = new Whistle("Peef");
+
+duckWhistle.sound();
+roosterWhistle.sound();
+duckWhistle.sound();
+
+
+
+Kvaak
+Peef
+Kvaak */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This is just an empty main method you can use to test
+        // the Whistle class. Try for example:
+
+//        Whistle duckWhistle = new Whistle("Kvaak");
+//        Whistle roosterWhistle = new Whistle("Peef");
+//
+//        duckWhistle.sound();
+//        roosterWhistle.sound();
+//        duckWhistle.sound();
+    }
+}
