63e6dc5946f37cc60d32b87a38fd01d493c0c554
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-24 15:40:02 +0100

    Update Door.java

--- Java-programming/Part-4/OOP-intro-ex-6/Door.java
+++ Java-programming/Part-4/OOP-intro-ex-6/Door.java
@@ -1,1 +1,19 @@
-a
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tom64
+ */
+public class Door {
+// making the new class
+
+    // not making a constructor, so java will make the default one, which creates an object, nothing more
+  
+    public void knock() {
+        System.out.println("Who's there?");
+    }
+}
