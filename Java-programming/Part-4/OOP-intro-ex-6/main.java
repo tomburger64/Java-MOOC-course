744e1067ffe2f2482daeb2b8a100f2534997219d
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-24 15:20:35 +0100

    Create main.java

---
+++ Java-programming/Part-4/OOP-intro-ex-6/main.java
@@ -0,0 +1,27 @@
+/* Create a class named Door. The door does not have any variables. Create for it a constructor with no parameters (or use the default constructor). After that, create a public void knock() method for the door that prints the message "Who's there?" when called.
+
+The door should work as follows.
+
+Door alexander = new Door();
+
+alexander.knock();
+alexander.knock();
+
+
+
+Who's there?
+Who's there? */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This is an empty main method
+        // You can use it to test your Door class. Try the following:
+
+//        Door alexander = new Door();
+//
+//        alexander.knock();
+//        alexander.knock();
+    }
+}
