7196a0a048b8f904546bea59d196f75ffaefc8a5
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-25 16:34:49 +0100

    UNFIFNISHED will come back to it later

--- Java-programming/Part-4/OOP-intro-ex-9/Debt.java
+++ Java-programming/Part-4/OOP-intro-ex-9/Debt.java
@@ -9,5 +9,20 @@
  * @author tom64
  */
 public class Debt {
+    private double balance;
+    private double interestRate;
     
+    public Debt(double initialBalance, double initialInterestRate) {
+        this.balance = initialBalance;
+        this.interestRate = initialInterestRate;
+    }
+    
+    public void printBalance() {
+        System.out.println(balance);
+    }
+    
+    public void waitOneYear() {
+        // ↓ fix this
+        //this.balance * this.interestRate;
+    }
 }
