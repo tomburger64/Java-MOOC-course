67badc75704a84291f931f57d749182641a4ac9f
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-25 16:22:50 +0100

    Update MainProgram.java

--- Java-programming/Part-4/OOP-intro-ex-8/MainProgram.java
+++ Java-programming/Part-4/OOP-intro-ex-8/MainProgram.java
@@ -106,14 +106,11 @@ value: 0 */
 public class MainProgram {
 
     public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(2);
+        DecreasingCounter counter = new DecreasingCounter(100);
 
         counter.printValue();
 
-        counter.decrement();
-        counter.printValue();
-
-        counter.decrement();
+        counter.reset();
         counter.printValue();
 
         counter.decrement();
