ca318354002b5f784ddd113596aaee840049a69f
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-28 18:35:08 +0100

    Update Film.java

--- Java-programming/Part-4/OOP-intro-ex-11/Film.java
+++ Java-programming/Part-4/OOP-intro-ex-11/Film.java
@@ -9,5 +9,18 @@
  * @author tom64
  */
 public class Film {
+    private String name;
+    private int ageRating;
     
+    public Film(String filmName, int filmAgeRating) {
+        this.name = filmName;
+        this.ageRating = filmAgeRating;
+    }
+    
+    public String name() {
+        return name;
+    }
+    public int ageRating() {
+        return ageRating;
+    }
 }
