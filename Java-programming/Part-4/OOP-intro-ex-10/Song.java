cfb6fe50b512dcb2536530209cd7ce512fe56b12
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-28 18:23:31 +0100

    Update Song.java

--- Java-programming/Part-4/OOP-intro-ex-10/Song.java
+++ Java-programming/Part-4/OOP-intro-ex-10/Song.java
@@ -9,5 +9,18 @@
  * @author tom64
  */
 public class Song {
+    private String name;
+    private int length;
     
+    public Song(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+    
+    public String name() {
+        return name;
+    }
+    public int length() {
+        return length;
+    }
 }
