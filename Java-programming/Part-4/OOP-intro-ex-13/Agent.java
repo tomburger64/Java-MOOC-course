0e0e7b3034e30518312a012b91412e62fc7d5c67
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-29 15:46:22 +0100

    no parentheses Agent.java

--- Java-programming/Part-4/OOP-intro-ex-13/Agent.java
+++ Java-programming/Part-4/OOP-intro-ex-13/Agent.java
@@ -37,7 +37,7 @@ public class Agent {
     }
     
     public String toString() {
-        return ("My name is " + this.lastName + ", " + this.firstName + " " + this.lastName);
+        return "My name is " + this.lastName + ", " + this.firstName + " " + this.lastName;
     }
     
 }
