1cef26ed580abaa1e6232d103bc6e7329da8dcb9
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-29 12:30:26 +0100

    Update Main.java

--- Java-programming/Part-4/OOP-intro-ex-12/Main.java
+++ Java-programming/Part-4/OOP-intro-ex-12/Main.java
@@ -1,41 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* Create the class Gauge. The gauge has the instance variable private int value, a constructor without parameters (sets the initial value of the meter variable to 0), and also the following four methods:
 
-/**
- *
- * @author tom64
- */
-public class Gauge {
-    private int value;
-    
-    public Gauge() {
-        int meter = 0;
-    }
-    
-    // methods
-    public void increase() {
-        if (this.value < 5) {
-            this.value++;
+    Method public void increase() grows the value instance variable's value by one. It does not grow the value beyond five.
+    Method public void decrease() decreases the value instance variable's value by one. It does not decrease the value to negative numbers.
+    Method public int value() returns the value variable's value.
+    Method public boolean full() returns true if the instance variable value has the value five. Otherwise, it returns false.
+
+An example of the class in use.
+
+Gauge g = new Gauge();
+
+while(!g.full()) {
+    System.out.println("Not full! Value: " + g.value());
+    g.increase();
+}
+
+System.out.println("Full! Value: " + g.value());
+g.decrease();
+System.out.println("Not full! Value: " + g.value());
+
+Sample output
+
+Not full! Value: 0
+Not full! Value: 1
+Not full! Value: 2
+Not full! Value: 3
+Not full! Value: 4
+Full! Value: 5
+Not full! Value: 4 */
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Test your Gauge class here
+
+        Gauge g = new Gauge();
+
+        while(!g.full()) {
+            System.out.println("Not full! Value: " + g.value());
+            g.increase();
         }
-    }
-    public void decrease() {
-        if (this.value < 0) {
-            this.value--;
-        }
-    }
-    
-    public int value() {
-        return this.value;
-    }
-    
-    public boolean full() {
-        if (this.value == 5) {
-            return true;
-        } else {
-            return false;
-        }
+
+        System.out.println("Full! Value: " + g.value());
+        g.decrease();
+        System.out.println("Not full! Value: " + g.value());
     }
 }
