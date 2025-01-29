f3856f73cca2c852362c9a19d0742d2095d8c03c
Tom <157364916+tomburger64@users.noreply.github.com>
2025-01-29 12:30:37 +0100

    Update Gauge.java

--- Java-programming/Part-4/OOP-intro-ex-12/Gauge.java
+++ Java-programming/Part-4/OOP-intro-ex-12/Gauge.java
@@ -9,5 +9,33 @@
  * @author tom64
  */
 public class Gauge {
+    private int value;
     
+    public Gauge() {
+        int meter = 0;
+    }
+    
+    // methods
+    public void increase() {
+        if (this.value < 5) {
+            this.value++;
+        }
+    }
+    public void decrease() {
+        if (this.value < 0) {
+            this.value--;
+        }
+    }
+    
+    public int value() {
+        return this.value;
+    }
+    
+    public boolean full() {
+        if (this.value == 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
