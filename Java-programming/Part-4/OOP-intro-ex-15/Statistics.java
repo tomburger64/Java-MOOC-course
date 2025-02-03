02c900378e72221b0ee815360867991dc100530b
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-03 23:24:34 +0100

    Update Statistics.java

--- Java-programming/Part-4/OOP-intro-ex-15/Statistics.java
+++ Java-programming/Part-4/OOP-intro-ex-15/Statistics.java
@@ -1,3 +1,15 @@
 
 public class Statistics {
+    private int count;
+    
+    public Statistics() {
+    }
+    
+    public void addNumber(int number) {
+        this.count++;
+    }
+    
+    public int getCount() {
+        return this.count;
+    }
 }
