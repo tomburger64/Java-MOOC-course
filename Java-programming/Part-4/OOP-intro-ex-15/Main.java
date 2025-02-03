623573c7b46176c0142697e5fb9900403a8f8fc1
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-04 00:12:03 +0100

    Update Main.java

--- Java-programming/Part-4/OOP-intro-ex-15/Main.java
+++ Java-programming/Part-4/OOP-intro-ex-15/Main.java
@@ -104,6 +104,27 @@ Enter numbers:
 -1
 Sum: 15 */
 
+/* Part 4: Multiple sums
+Change the previous program so that it also calculates the sum of even and odd numbers.
+
+NOTE: Define three Statistics objects in the program. Use the first to calculate the sum of all numbers, the second to calculate the sum of even numbers, and the third to calculate the sum of odd numbers.
+
+For the test to work, the objects must be created in the main program in the order they were mentioned above (i.e., first the object that sums all the numbers, then the one that sums the even ones, and then finally the one that sums the odd numbers)!
+
+NOTE: Do not change the Statistics class in any way!
+
+The program should work as follows:
+
+Enter numbers:
+4
+2
+5
+2
+-1
+Sum: 13
+Sum of even numbers: 8
+Sum of odd numbers: 5 */
+
 
 import java.util.Scanner;
 
