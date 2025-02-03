aea21bfcecc1c5a97f4cfa651a78674c52e54801
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-03 22:58:37 +0100

    Create Main.java

---
+++ Java-programming/Part-4/OOP-intro-ex-15/Main.java
@@ -0,0 +1,57 @@
+/* Part 1: Count
+Create a class Statistics that has the following functionality (the file for the class is provided in the exercise template):
+
+a method addNumber adds a new number to the statistics
+a method getCount tells the number of added numbers
+The class does not need to store the added numbers anywhere, it is enough for it to remember their count. At this stage, the addNumber method can even neglect the numbers being added to the statistics, since the only thing being stored is the count of numbers added.
+
+The method's body is the following:
+
+public class Statistics {
+    private int count;
+
+    public Statistics() {
+        // initialize the variable numberCount here
+    }
+
+    public void addNumber(int number) {
+        // write code here
+    }
+
+    public int getCount() {
+        // write code here
+    }
+}
+The following program introduces the class' use:
+
+public class MainProgram {
+    public static void main(String[] args) {
+        Statistics statistics = new Statistics();
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
+    }
+}
+The program prints the following:
+
+Sample output
+Count: 4 */
+
+
+import java.util.Scanner;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // you can write test code here
+        // however, remove all unnecessary code when doing the final parts of the exercise
+
+        // In order for the tests to work, the objects must be created in the
+        // correct order in the main program. First the object that tracks the total
+        // sum, secondly the object that tracks the sum of even numbers, 
+        // and lastly the one that tracks the sum of odd numbers!
+    }
+}
