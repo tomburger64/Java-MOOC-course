2e3b754f6631be8da1bc4bda4bbbd931270a7e37
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-04 17:35:06 +0100

    Update Main.java

--- Java-programming/Part-4/OOP-intro-ex-16/Main.java
+++ Java-programming/Part-4/OOP-intro-ex-16/Main.java
@@ -193,9 +193,26 @@ public class MainProgram {
     public static void main(String[] args) {
         // Scanner scanner = new Scanner(System.in);
         
-        PaymentCard card = new PaymentCard(10);
-        System.out.println("Paul: " + card);
-        card.addMoney(-15);
-        System.out.println("Paul: " + card);
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+        
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        
+        System.out.println(paulsCard.toString());
+        System.out.println(mattsCard.toString());
+        
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+        
+        System.out.println(paulsCard.toString());
+        System.out.println(mattsCard.toString());
+        
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        mattsCard.addMoney(50);
+        
+        System.out.println(paulsCard.toString());
+        System.out.println(mattsCard.toString());
     }
 }
