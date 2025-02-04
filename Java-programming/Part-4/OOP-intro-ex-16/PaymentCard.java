f569605ea34df55e1c0aca3a0444a9580de9e132
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-04 16:53:53 +0100

    Create PaymentCard.java

---
+++ Java-programming/Part-4/OOP-intro-ex-16/PaymentCard.java
@@ -0,0 +1,13 @@
+
+public class PaymentCard {
+    private double balance;
+    
+    public PaymentCard(double openingBalance) {
+        this.balance = openingBalance;
+    }
+    
+    public String toString() {
+        return "The card has a balance of " + this.balance + " euros";
+    }
+    
+}
