97b2a63f20de7108a07307aa881137acf05c2d12
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-10 16:13:22 +0100

    Update Items.java

--- Java-programming/Part-4/objects-in-lists-ex-1/Items.java
+++ Java-programming/Part-4/objects-in-lists-ex-1/Items.java
@@ -4,11 +4,26 @@ import java.util.Scanner;
 
 public class Items {
 
+    // haven't coded in a whlie, had to cheat to know how to do a for each loop and to to call a mathod using the for each loop
     public static void main(String[] args) {
         // implement here your program that uses the class Item
 
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Name: ");
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            } else {
+                items.add(new Item(input));
+            }
+        }
+        
+        for (Item item : items) {
+            System.out.println(item.toString());
+        }
     }
 }
