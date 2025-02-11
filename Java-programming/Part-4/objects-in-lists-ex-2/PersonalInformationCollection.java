a6acfce4dc8a1c92f7d34c79864ef58853756c05
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-11 15:07:03 +0100

    Update PersonalInformationCollection.java

--- Java-programming/Part-4/objects-in-lists-ex-2/PersonalInformationCollection.java
+++ Java-programming/Part-4/objects-in-lists-ex-2/PersonalInformationCollection.java
@@ -9,6 +9,29 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("First name: ");
+            String firstName = scanner.nextLine();
+            
+            if (firstName.equals("")) {
+                break;
+            }
+            
+            System.out.println("Last name: ");
+            String lastName = scanner.nextLine();
+            
+            System.out.println("Identification number: ");
+            String idNum = scanner.nextLine();
+            
+            infoCollection.add(new PersonalInformation(firstName, lastName, idNum));
+        }
+        
+        for (PersonalInformation Info : infoCollection) {
+            System.out.println(Info.getFirstName() + " " + Info.getLastName());
+        }
 
     }
 }
+
+// starting to get a hang of it, it's really similar to the stuff I did in my JS repo with the temporary variables (that really helped)
