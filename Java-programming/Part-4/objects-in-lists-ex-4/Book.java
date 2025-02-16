fc455cf265c57718f9f5e6cf062438d729a9d4f6
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-16 15:19:45 +0100

    Create Book.java

---
+++ Java-programming/Part-4/objects-in-lists-ex-4/Book.java
@@ -0,0 +1,21 @@
+
+public class Book {
+    private String title;
+    private int pages;
+    private int pubYear;
+    
+    public Book(String title, int pages, int pubYear) {
+        this.title = title;
+        this.pages = pages;
+        this.pubYear = pubYear;
+    }
+    
+    // Everything
+    public String returnE() {
+        return this.title + ", " + this.pages + ", " + this.pubYear;
+    }
+    
+    public String returnTitle() {
+        return this.title;
+    }
+}
