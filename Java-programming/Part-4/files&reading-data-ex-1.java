88a4306988ea2fb6fb741326bb9108904e800a2b
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-16 15:39:12 +0100

    Update files&reading-data-ex-1.java

--- Java-programming/Part-4/files&reading-data-ex-1.java
+++ Java-programming/Part-4/files&reading-data-ex-1.java
@@ -8,6 +8,17 @@ public class NumberOfStrings {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int count = 0;
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("end")) {
+                System.out.println(count);
+                break;
+            } else {
+                count++;
+            }
+        }
     }
 }
         
