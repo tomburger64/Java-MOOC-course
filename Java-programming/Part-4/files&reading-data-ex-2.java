b57194f207aaf83a90e3d3ef2587af0ee2751feb
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-16 15:47:48 +0100

    Create files&reading-data-ex-2.java

---
+++ Java-programming/Part-4/files&reading-data-ex-2.java
@@ -0,0 +1,24 @@
+/* Write a program that reads strings from the user until the user inputs the string "end". As long as the input is not "end", the program should handle the input as an integer and print the cube of the number provided (i.e., number _ number _ number). Below are some sample outputs
+
+3
+27
+-1
+-1
+11
+1331
+end
+
+
+
+end */
+
+
+import java.util.Scanner;
+
+public class Cubes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
