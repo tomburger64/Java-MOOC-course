c0d9d5071dd5c76a57e3752350a845a914c2c7b2
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-16 15:15:57 +0100

    Update Main.java

--- Java-programming/Part-4/objects-in-lists-ex-4/Main.java
+++ Java-programming/Part-4/objects-in-lists-ex-4/Main.java
@@ -57,6 +57,7 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
+        ArrayList<Book> books = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -72,13 +73,21 @@ public class Main {
             
             System.out.println("Publication year: ");
             int pubYear = Integer.valueOf(scanner.nextLine());
+            
+            books.add(new Book(title, pages, pubYear));
         }
         
         System.out.println("What information will be printed? ");
         String input = scanner.nextLine();
         
         if (input.equals("everything")){
-            System.out.println("");
+            for (Book book : books) {
+                System.out.println(book.returnE());
+            }
+        } else if (input.equals("name")) {
+            for (Book book : books) {
+                System.out.println(book.returnTitle());
+            }
         }
     }
 }
