4682934a7b9ca151a93efbd2174f4a9363334e54
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-16 15:34:20 +0100

    Create files&reading-data-ex-1.java

---
+++ Java-programming/Part-4/files&reading-data-ex-1.java
@@ -0,0 +1,13 @@
+/* Write a program that reads strings from the user until the user inputs the string "end". At that point, the program should print how many strings have been read. The string "end" should not be included in the number strings read. You can find some examples below of how the program works. */
+
+
+import java.util.Scanner;
+
+public class NumberOfStrings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
+        
