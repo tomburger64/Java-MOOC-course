cbf2ac72341c1f82546b8c546785f5ac141fa880
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-17 18:36:49 +0100

    Create files&reading-data-ex-4.java

---
+++ Java-programming/Part-4/files&reading-data-ex-4.java
@@ -0,0 +1,26 @@
+/* Write a program that prints the contents of a file called "data.txt", such that each line of the file is printed on its own line.
+
+If the file content looks like so:
+
+Sample data
+In a
+world
+
+Then the program should print the following:
+
+Sample output
+In a
+world */
+
+//                                                                            data.txt
+//                                                                               ↑
+// important: in the project folder, data.txt is 3 parents above the java file (src > main > java → program.java)
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class PrintingAFile {
+
+    public static void main(String[] args) {
+
+    }
+}
