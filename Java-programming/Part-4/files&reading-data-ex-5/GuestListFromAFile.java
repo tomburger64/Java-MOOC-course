40bc1ed6f8ec782519226db0bb81d8799cfd757d
Tom <157364916+tomburger64@users.noreply.github.com>
2025-02-17 20:26:28 +0100

    Create GuestListFromAFile.java

---
+++ Java-programming/Part-4/files&reading-data-ex-5/GuestListFromAFile.java
@@ -0,0 +1,55 @@
+/* Write a program that asks the user for a string, and then prints the contents of a file with a name matching the string provided. You may assume that the user provides a file name that the program can find.
+
+The exercise template contains the files "data.txt" and "song.txt", which you may use when testing the functionality of your program. The output of the program can be seen below for when a user has entered the string "song.txt". The content that is printed comes from the file "song.txt". Naturally, the program should also work with other filenames, assuming the file can be found.
+
+Sample output
+Which file should have its contents printed?
+song.txt
+No option for duality
+The old is where we come
+Clockspeed is fast, but we'll survive
+The new will overcome
+We are challengers, not followers
+We take the ball to build
+Easy safe services
+Are here to stay
+
+Value for society
+Value for life
+For you and me
+Tieto is here allright! */
+
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GuestListFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        ArrayList<String> list = new ArrayList<>();
+        // implement reading the file here.
+        System.out.println("");
+
+        System.out.println("Enter names, an empty line quits.");
+        while (true) {
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            if (list.contains(name)) {
+                System.out.println("The name is on the list.");
+            } else {
+                System.out.println("The name is not on the list.");
+            }
+        }
+
+        System.out.println("Thank you!");
+    }
+}
